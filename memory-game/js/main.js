ed6e6428073e069db3f872832ef315fea13f220c
Emma Phillips <emmaphillips@Emmas-MacBook-Pro.local>
2020-03-05 12:39:08 -0600

    created game logic if/else

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,19 @@
-console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+let cardTwo = cards[];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+
+if (cardsInPlay.length = 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
